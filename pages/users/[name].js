9b3d53fc5a7b6e47f17dcdaba73fae0496dcb758
EmersonNeves <emersonneves@outlook.com>
2021-08-13 22:21:37 -0300

    Alterações na página visitada

--- pages/users/[name].js
+++ pages/users/[name].js
@@ -1,20 +1,35 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { useRouter } from "next/router";
 import {
   AlurakutMenu,
   AlurakutProfileSidebarMenuDefault,
+  OrkutNostalgicIconSet,
 } from "../../src/lib/AluraCommons";
 import MainGrid from "../../src/components/MainGrid";
 import Box from "../../src/components/Box";
 
 export default function Profile() {
   const router = useRouter();
+  const [user, setUser] = useState('')
 
   const {
     query: { name },
   } = router;
 
   const githubUser = name;
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${githubUser}`)
+      .then((user) => {
+        return user.json();
+      })
+      .then((response) => {
+        setUser(response.name);
+      });
+  }, []);
+
+
+
 
   return (
     <div>
@@ -34,6 +49,18 @@ export default function Profile() {
             </p>
             <hr />
             <AlurakutProfileSidebarMenuDefault />
+          </Box>
+        </div>
+        <div className="welcomeArea" style={{ gridArea: "welcomeArea" }}>
+          <Box>
+            <h1 className="title">{user}</h1>
+            <OrkutNostalgicIconSet />
+            <p style={{ paddingTop: "20px", fontWeight: "bold" }}>
+              Visualizações do perfil:
+            </p>
+            <div style={{ color: "#5A5A5A" }}>
+              <p>Total: 0, Última semana: 0, Ontem: 0</p>
+            </div>
           </Box>
         </div>
       </MainGrid>
--- src/lib/AluraCommons.js
+++ src/lib/AluraCommons.js
@@ -29,7 +29,7 @@ export function AlurakutMenu({ githubUser }) {
   return (
     <AlurakutMenu.Wrapper isMenuOpen={isMenuOpen}>
       <div className="container">
-        <AlurakutMenu.Logo src={`${BASE_URL}/logo.svg`} />
+        <AlurakutMenu.Logo  src={`${BASE_URL}/logo.svg`} />
 
         <nav style={{ flex: 1 }}>
           {[{ name: 'Inicio', slug: '/'}, {name: 'Amigos', slug: '/amigos'}, {name: 'Comunidades', slug: '/comunidades'}].map((menuItem) => (
@@ -277,7 +277,7 @@ export function OrkutNostalgicIconSet(props) {
         const total = props[slug] ? props[slug] : 2;
         return (
           <>
-          <li key={`orkut__icon_set__${slug}`}>
+          <li key={`orkut__icon_set__${slug}+${slug}`}>
             <span className="OrkutNostalgicIconSet__title">
               {name}
             </span>
